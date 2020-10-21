12b36a9a52885d0c95d01a0e8cc72d0d9e7e389f
Iamsiyanda <s.mayekiso@gmail.com>
2020-10-21 13:17:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const questions = [
   // questions to user using "inquirer"
   {
     type: "input",
-    message: "What is your GitHub user name?",
+    message: "What is your GitHub user-name?",
     name: "username"
   },
 
@@ -22,7 +22,7 @@ const questions = [
 
   {
     type: "input",
-    message: "What is your repo called?",
+    message: "What is your repository called?",
     name: "repo",
     default: "GoodREADMEGenerator"
   },
@@ -44,7 +44,7 @@ const questions = [
 
   {
     type: "input",
-    message: "Write instructions for using your project.",
+    message: "Create instructions for using your project.",
     name: "usage",
     default:
       "1.Run node index.js 2.Answers the questions 3.The README.md file will be created. "
@@ -53,16 +53,15 @@ const questions = [
   {
     type: "input",
     message:
-      "please enter git hub user names of the contributor if any (If there are mulitple contributor, seperate names with comma and no space! )",
-    name: "contributors",
-    default: " Robert McKenney, Abdul Amoud and Igor Calvacante"
+      "please enter git hub user names of the contributor",
+    default: "Siyanda Mayekiso"
   },
 
   {
     type: "input",
-    message: "Provide examples on how to run tests.",
+    message: "Provide guides on how to simulate tests.",
     name: "test",
-    default: "Insert your tests sample here..."
+    default: "Insert your test simulation here..."
   }
 ];
 //answers.username
